d3770834ef6af8f423006d4b4409e8ad3eddc933
fmccready <fmccready@gmail.com>
2017-03-29 20:59:26 -0500

    Setup basic express server.

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/dist'));
+/*
+app.use('/assets', express.static('dist/assets'));
+app.all('/inline.js', (req, res) =>{
+    res.status(200).sendFile(__dirname + '/dist/inline.js');
+});
+app.all('/main.bundle.js', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/main.bundle.js');
+});
+app.all('/inline.map', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/inline.map');
+});
+app.all('/main.map', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/main.map');
+});
+app.all('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/dist/index.html');
+});
+*/
+
+app.listen(3000, function(){
+    console.log('listening on port 3000');
+});
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+module.exports = app;
