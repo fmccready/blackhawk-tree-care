c5326f4b3047fc6d930d3df0086575a23c52ea7f
fmccready <fmccready@gmail.com>
2017-03-29 22:01:24 -0500

    Remove console.log.

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.all('/', (req, res) => {
 */
 
 app.listen(process.env.port || 3000, function(){
-    console.log('listening on port 3000');
 });
 app.use(function(err, req, res, next){
     console.error(err.stack);
