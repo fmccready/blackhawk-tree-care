a3436e2a524f6c856a07fb6fc0ae97e6733f3670
fmccready <fmccready@gmail.com>
2017-04-01 13:33:08 -0500

    Remove console log.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-console.log(process.env.PORT);
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,24 +8,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/dist'));
-/*
-app.use('/assets', express.static('dist/assets'));
-app.all('/inline.js', (req, res) =>{
-    res.status(200).sendFile(__dirname + '/dist/inline.js');
-});
-app.all('/main.bundle.js', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/main.bundle.js');
-});
-app.all('/inline.map', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/inline.map');
-});
-app.all('/main.map', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/main.map');
-});
-app.all('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/dist/index.html');
-});
-*/
 
 app.listen(process.env.PORT || 3000);
 app.use(function(err, req, res, next){
